84da12762a3f94de48dbc51c31343ed10c58b666
gitdxj <frdxj98@gmail.com>
2021-09-03 18:32:53 +0200

    generate dataloader for training set and test set; Simple Convolutional Neural Network

--- covidata.py
+++ covidata.py
@@ -2,9 +2,13 @@ import os
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+# from sklearn.model_selection import train_test_split
 
+CLASS_MAPPING = {'COVID':0, 'Lung_Opacity':1, 'Normal':2, 'Viral Pneumonia':3}
 
 def readData(path="./COVID-19_Radiography_Dataset/", classes=['COVID', 'Lung_Opacity', 'Normal', 'Viral Pneumonia']):
     """
@@ -19,7 +23,7 @@ def readData(path="./COVID-19_Radiography_Dataset/", classes=['COVID', 'Lung_Opa
         for imgPath in glob.glob(os.path.join(path, className, '*')):
             # get the path of all files in path/className
             X.append(imgPath)
-            y.append(className)
+            y.append(CLASS_MAPPING[className])
     return np.asarray(X), np.asarray(y)
 
 
@@ -32,10 +36,29 @@ class CovidDataset(Dataset):
         return len(self.X)
 
     def __getitem__(self, index):
-        imgPath = self.X[index]
-        img = plt.imread(imgPath)
+        img_path = self.X[index]
+        # img = plt.imread(img_path)
+        img = transforms.ToTensor()(Image.open(img_path).convert('L'))
         label = self.y[index]
-        return (img, label)
+        return img.view(1, 299, 299), torch.tensor(label)
+
+
+def createDataLoader(X, y, test_ratio=0.3):
+    """
+    :param X: The list of image paths
+    :param y: The list of labels
+    :param test_ratio: the proportion of test data
+    :return: data loaders of training set and test set
+    """
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio)
+    # train_dataset = CovidDataset(X_train, y_train)
+    # test_dataset = CovidDataset(X_test, y_test)
+    # train_loader = DataLoader(train_dataset, batch_size=32)
+    # test_loader = DataLoader(test_dataset, batch_size=32)
+    # return train_loader, test_loader
+    dataset = CovidDataset(X, y)
+    loader = DataLoader(dataset, 32)
+    return loader, loader
 
 
 if __name__ == '__main__':
@@ -44,7 +67,9 @@ if __name__ == '__main__':
     print(f'y.shape {len(y)} - type(y) {type(y)}')
 
     dataset = CovidDataset(X, y)
-    img, label = dataset[12]
-    print(type(img), label)
-    print(img.shape)
+    img = dataset[10]
+    print(img)
+    for i in range(len(dataset)):
+        img, label = dataset[i]
+
 
---
+++ simplecnn.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn.functional as F
+import covidata
+
+class SimpleCNN(torch.nn.Module):
+    """
+    Simple Convolutional Neural Network
+    """
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=10, kernel_size=7)
+        self.conv2 = torch.nn.Conv2d(in_channels=10, out_channels=20, kernel_size=7)
+        self.conv3 = torch.nn.Conv2d(in_channels=20, out_channels=30, kernel_size=7)
+        self.pooling = torch.nn.MaxPool2d(3)
+        self.fc1 = torch.nn.Linear(1920, 320)
+        self.fc2 = torch.nn.Linear(320, 40)
+        self.fc3 = torch.nn.Linear(40, 4)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = F.relu(self.pooling(self.conv1(x)))
+        x = F.relu(self.pooling(self.conv2(x)))
+        x = F.relu(self.pooling(self.conv3(x)))
+        x = x.view(batch_size, -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+
+def train(model, epoch, train_loader, optimizer, criterion, device):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        inputs, target = inputs.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # forward + backward + update
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if batch_idx % 50 == 49:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss))
+            running_loss = 0.0
+
+
+def test(model, test_loader, device):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            inputs, target = data
+            inputs, target = inputs.to(device), target.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
+    print('Accuracy on test set: %d %% [%d/%d]' % (100 * correct / total, correct, total))
+
+
+if __name__ == '__main__':
+
+    model = SimpleCNN()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    X, y = covidata.readData()
+    train_loader, test_loader = covidata.createDataLoader(X, y)
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+    for epoch in range(10):
+        train(model, epoch, train_loader, optimizer, criterion, device)
+        test(model, test_loader, device)
