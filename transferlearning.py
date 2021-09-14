5e6a341076249a33cab108470134fa8ff590d9b4
gitdxj <frdxj98@gmail.com>
2021-09-14 18:54:21 +0200

    Implement transfer learning: use pretrained models for classification

---
+++ transferlearning.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torchvision
+import covidata
+import torchvision.transforms as transforms
+
+
+def train(model, epoch, train_loader, optimizer, criterion, device):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        inputs, target = inputs.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # forward + backward + update
+        outputs = model(inputs)
+        loss = criterion(outputs, target.long())
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if batch_idx % 50 == 49:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss))
+            running_loss = 0.0
+
+
+def test(model, test_loader, device):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            inputs, target = data
+            inputs, target = inputs.to(device), target.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
+    print('Accuracy on test set: %d %% [%d/%d]' % (100 * correct / total, correct, total))
+
+
+if __name__ == '__main__':
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = torchvision.models.vgg16(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+
+    model.fc = nn.Sequential(
+        nn.Linear(2048, 64),
+        nn.ReLU(inplace=True),
+        nn.Linear(64, 3)
+    ).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.fc.parameters())
+
+    model.to(device)
+
+    X, y = covidata.readData()
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
+        transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15, resample=False, fillcolor=0),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+    train_loader, test_loader = covidata.createDataLoader(X, y, 32, 0.2)
+
+    for epoch in range(10):
+        train(model, epoch, train_loader, optimizer, criterion, device)
+        test(model, test_loader, device)
