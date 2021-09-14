38ef9ecf482f1c74f5e17d9de5b8251420a93d85
gitdxj <frdxj98@gmail.com>
2021-09-14 19:13:57 +0200

    convert images to 3 channels to fit VGG16

--- transferlearning.py
+++ transferlearning.py
@@ -63,6 +63,7 @@ if __name__ == '__main__':
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
         transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15, resample=False, fillcolor=0),
         transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     ])
     train_loader, test_loader = covidata.createDataLoader(X, y, 32, 0.2)
