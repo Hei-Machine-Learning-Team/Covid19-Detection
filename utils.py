bug fixes on accuracy calculation

utils.py
@@ -37,7 +37,7 @@ def test(model, test_loader, device):
             _, predicted = torch.max(outputs.data, dim=1)
             total += target.size(0)
             correct += (predicted == target).sum().item()
-    accuracy = 100 * correct / total, correct, total
+    accuracy = 100 * correct / total
     print('Accuracy on test set: %d %% [%d/%d]' % (100 * correct / total, correct, total))
     return accuracy
 