816150b47094f2a68b9f9655d0da3d7cce49af90
gitdxj <frdxj98@gmail.com>
2021-09-22 18:49:09 +0200

    modification on evaluation script

--- evaluation.py
+++ evaluation.py
@@ -5,8 +5,9 @@ from utils import eval_model
 from transferlearning import initialize_model, get_transforms
 
 if __name__ == '__main__':
+    folder = "../savedmodels/alexnet/"
+    model_path = folder + "save.pt"
     model_name = "alexnet"
-    model_path = "./savedmodels/alexnet"
     model, input_size = initialize_model(model_name, 3, False, False)
     model.load_state_dict(torch.load(model_path))
     model.eval()
@@ -14,7 +15,9 @@ if __name__ == '__main__':
     _, test_transform = get_transforms(input_size)
 
     # create dataloader for test set
-    X_test = np.load()
-    y_test = np.load()
+    X_test = np.load(folder + "Xtest.npy")
+    y_test = np.load(folder + "ytest.npy")
     test_loader = createTestLoader(X_test, y_test, 32, test_transform)
     confusion_matrix = eval_model(model, test_loader)
+    print("confusion matrix:")
+    print(confusion_matrix)
